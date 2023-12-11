6461f283b878889d51307010d8c8b16bcabe7e1b
Elliott Chong <elliottchong16@gmail.com>
2023-12-11 12:02:48 +0800

    try db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,36 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import type { GetServerSideProps } from "next";
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { getServerAuthSession } from "@/server/auth";
+import { db } from "@/server/db";
+import type { FormSchema } from "@prisma/client";
 
-export default function Home() {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const forms = await db.formSchema.findMany({});
+  return {
+    props: { forms: JSON.parse(JSON.stringify(forms)) as FormSchema[] },
+  };
+  const session = await getServerAuthSession(ctx);
+  if (session?.user) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+};
+
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+export default function Home({ forms }: Props) {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <pre>{JSON.stringify(forms, null, 2)}</pre>
       <Card className="w-[300px]">
         <CardHeader>
           <CardTitle>Welcome to INC Form 🔥!</CardTitle>
@@ -26,18 +50,3 @@ export default function Home() {
     </div>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = await getServerAuthSession(ctx);
-  if (session?.user) {
-    return {
-      redirect: {
-        destination: "/dashboard",
-        permanent: false,
-      },
-    };
-  }
-  return {
-    props: {},
-  };
-};
