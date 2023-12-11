2374d72abc4f164896f21f8ad255f9c75e0ad64f
Elliott Chong <elliottchong16@gmail.com>
2023-12-11 10:54:07 +0800

    github action

---
+++ sst.config.ts
@@ -0,0 +1,29 @@
+import type { SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "janked-form-builder",
+      region: "ap-southeast-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          DATABASE_URL: process.env.DATABASE_URL!,
+          NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
+          NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL!,
+          GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
+          GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
